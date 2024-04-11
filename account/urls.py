0cb5c4b5db64dceb3dacf16fe65f5ab81c7d86f6
Artemiy <mihaylovartem54@gmail.com>
2024-04-11 10:27:30 +0300

    Payment and password reset added

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import path, reverse_lazy
 from . import views
 
 
@@ -17,5 +18,24 @@ urlpatterns = [
     path('profile-management/', views.profile_user, name='profile-management'),
     path('delete-user/', views.delete_user, name='delete-user'),
 
+    # Password reset
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='account/password/password-reset.html',
+        email_template_name='account/password/password_reset_email.html',
+        success_url=reverse_lazy('account:password_reset_done')),
+         name='password_reset'),
+
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='account/password/password-reset-done.html'),
+         name='password_reset_done'),
+
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='account/password/password-reset-confirm.html',
+        success_url=reverse_lazy('account:password_reset_complete')),
+         name='password_reset_confirm'),
+
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='account/password/password-reset-complete.html'),
+         name='password_reset_complete'),
 
 ]
--- bigcorp/settings.py
+++ bigcorp/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
     'account',
+    'payment',
 ]
 
 MIDDLEWARE = [
